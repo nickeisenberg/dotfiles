25a8a10876b7c95d52e68cddbfe2cd4509d0a560
nickeisenberg <nickeisenberg@gmail.com>
2023-09-27 19:15:37 -0700

    inc the bringtness by 10%

--- .config/qtile/scripts/inc_brightness.sh
+++ .config/qtile/scripts/inc_brightness.sh
@@ -3,7 +3,7 @@
 inc_bright(){
 
     local val=$(xrandr --verbose | awk '/Bright/ {print $2}')
-    local inv_val=.05
+    local inv_val=.1
     local newval=$(echo "$val + $inv_val" | bc);
     xrandr --output eDP-1 --brightness $newval;
 
