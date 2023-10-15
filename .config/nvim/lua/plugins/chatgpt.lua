90ad862154e2df6ea08a336a8c6bc12c81e79b06
nickeisenberg <nickeisenberg@gmail.com>
2023-10-15 09:17:56 -0700

    chapt gpt

--- .config/nvim/lua/plugins/chatgpt.lua
+++ .config/nvim/lua/plugins/chatgpt.lua
@@ -3,6 +3,25 @@ return {
     event = "VeryLazy",
     config = function()
       require("chatgpt").setup({
+
+      --   popup_layout = {
+      --     default = "center",
+      --     center = {
+      --       width = "90%",
+      --       height = "90%",
+      --     },
+      --     right = {
+      --       width = "30%",
+      --       width_settings_open = "50%",
+      --     },
+      --   },
+
+      --   system_window = {
+      --     size = {
+      --       width = 200
+      --     }
+      --   },
+
     })
     end,
     dependencies = {
