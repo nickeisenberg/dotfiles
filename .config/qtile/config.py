7cd5cc4f72e63c65c5eba55f1f952c8a7d25aba0
nickeisenberg <nickeisenberg@gmail.com>
2023-10-30 11:30:53 -0700

    matplotlib always will open in a floating window

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -372,12 +372,13 @@ floating_layout_theme = {
     "border_normal": widget_background,
     "float_rules": [
         *layout.Floating.default_float_rules,
-        Match(wm_class="confirmreset"),  # gitk
-        Match(wm_class="makebranch"),  # gitk
-        Match(wm_class="maketag"),  # gitk
-        Match(wm_class="ssh-askpass"),  # ssh-askpass
-        Match(title="branchdialog"),  # gitk
-        Match(title="pinentry"),
+        # Match(wm_class="confirmreset"),  # gitk
+        # Match(wm_class="makebranch"),  # gitk
+        # Match(wm_class="maketag"),  # gitk
+        # Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(wm_class="matplotlib"),  # ssh-askpass
+        # Match(title="branchdialog"),  # gitk
+        # Match(title="pinentry"),
     ]
 }
 
