70bb72d6483dfd9671457288dd33e9d746a60711
nickeisenberg <nickeisenberg@gmail.com>
2024-01-22 22:43:25 -0800

    remove the auto close dap ui functoins from the config

--- .config/nvim/lua/plugins/dap-ui.lua
+++ .config/nvim/lua/plugins/dap-ui.lua
@@ -12,21 +12,22 @@ return {
   },
   -- automatically open/close the DAP UI when starting/stopping the debugger
   config = function()
-    local dap, dapui = require("dap"), require("dapui")
-
+    local dapui = require("dapui")
     dapui.setup()
 
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
+    local dap = require("dap")
+
+    -- dap.listeners.before.attach.dapui_config = function()
+    --   dapui.open()
+    -- end
+    -- dap.listeners.before.launch.dapui_config = function()
+    --   dapui.open()
+    -- end
+    -- dap.listeners.before.event_terminated.dapui_config = function()
+    --   dapui.close()
+    -- end
+    -- dap.listeners.before.event_exited.dapui_config = function()
+    --   dapui.close()
+    -- end
   end,
 }
