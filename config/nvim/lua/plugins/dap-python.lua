c8c5b3b53d61926572f58968eba10c14ebf16457
Nicholas T. Eisenberg <DEVNET\eisenbergn@stl-b324166.dev.net>
2024-02-15 20:21:53 -0800

    nvim dap debugpy file path

--- config/nvim/lua/plugins/dap-python.lua
+++ config/nvim/lua/plugins/dap-python.lua
@@ -2,7 +2,7 @@ return {
   "mfussenegger/nvim-dap-python",
   dependencies = "mfussenegger/nvim-dap",
   config = function()
-    local debugpyPythonPath = "/home/nicholas/.venv/debugpy/bin/python"
+    local debugpyPythonPath = os.getenv("HOME") .. "/.venv310/debugpy/bin/python"
     print(debugpyPythonPath)
   	require("dap-python").setup(debugpyPythonPath)
   end,
