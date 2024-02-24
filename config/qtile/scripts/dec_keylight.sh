6823b3438e7f9341866d769666b31a8a5c16a037
nickeisenberg <nickeisenberg@gmail.com>
2024-02-24 12:47:59 -0800

    keylight qtile script

--- config/qtile/scripts/dec_keylight.sh
+++ config/qtile/scripts/dec_keylight.sh
@@ -1,27 +1,10 @@
-# #! /bin/bash
-# 
-# function decrease_keylight() {
-#     
-#     current=$(cat /sys/class/leds/tpacpi::kbd_backlight/brightness)
-# 
-#     new=$(echo $current - 1 | bc)
-# 
-#     if [ "$new" -lt 0 ]
-#         then
-#             ((new+=1))
-#     fi
-# 
-#     sudo echo $new | tee /sys/class/leds/tpacpi::kbd_backlight/brightness > /dev/null
-# 
-# }  
-# 
-# decrease_keylight
+#! /bin/bash
 
 # to get this to work with no sudo password, I added
 # nicholas ALL=(ALL:ALL) NOPASSWD: /home/nicholas/Dotfiles/.config/qtile/scripts/dec_keylight.sh
 # /etc/sudoers.d/sudo_no_password
 
-decrease_brightness() {
+decrease_keylight() {
     # Define common backlight control paths
     declare -a paths=(
         "/sys/class/leds/tpacpi::kbd_backlight/brightness" # Path for ThinkPad
