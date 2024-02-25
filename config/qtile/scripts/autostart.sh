2d061c897c7ab9912bf2a36f5c85fe894e0d02bd
Nicholas Eisenberg <DEVNET\eisenbergn@stl-b324166.dev.net>
2024-02-25 10:15:26 -0800

    I have set the wallpaper to the rosepine colorcode

--- config/qtile/scripts/autostart.sh
+++ config/qtile/scripts/autostart.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
+#----------
 # capslock to control
+#----------
 setxkbmap -option ctrl:nocaps &
 
 # picom --experimental-backends --backend glx --xrender-sync-fence &
 
-feh --bg-scale ~/dots_ubuntu/config/qtile/rosepine.png
+#----------
+# Wallpaper
+#----------
+# feh --bg-scale ~/dots_ubuntu/config/qtile/rosepine.png
+xsetroot -solid '#191724'
+#----------
 
+#----------
 # set up the dual monitor
 # Use xrandr --verbose to ge the monitor names
+#----------
 sleep 1
 
 var=$(xrandr | awk '/HDMI-1-0/ {print $2}')
