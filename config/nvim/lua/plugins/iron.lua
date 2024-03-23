f69a9b92183b7f48ba96483edbccb006bc4ea0a1
nickeisenberg <nickeisenberg@gmail.com>
2024-03-23 10:18:08 -0700

    configured iron nvim

--- config/nvim/lua/plugins/iron.lua
+++ config/nvim/lua/plugins/iron.lua
@@ -12,37 +12,52 @@ return {
             command = { "bash" }
           }
         },
-        repl_open_cmd = view.split.vertical.botright(50),
+        repl_open_cmd = view.split.vertical.botright("%40"),
       },
       keymaps = {
-        send_motion = "<space>sc",
-        visual_send = "<space>sc",
-        send_file = "<space>sf",
         send_line = "<space>sl",
-        send_until_cursor = "<space>su",
-        send_mark = "<space>sm",
-        mark_motion = "<space>mc",
-        mark_visual = "<space>mc",
-        remove_mark = "<space>md",
-        cr = "<space>s<cr>",
-        interrupt = "<space>s<space>",
-        exit = "<space>sq",
-        clear = "<space>cl",
+        visual_send = "<space>sv",
+        send_file = "<space>sf",
+        exit = "<space>rq",
+        clear = "<space>rc",
       },
       highlight = {
         italic = true
       },
-      ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+      ignore_blank_lines = true,
     }
 
-    vim.keymap.set('n', '<space>sp', function()
-      vim.cmd('normal! vip') -- Select the current paragraph in visual mode
-      iron.visual_send() -- Send the selected text to the REPL
+    
+    vim.keymap.set('n', '<space>rh', function()
+      iron.setup(
+        {
+          config = {
+            repl_open_cmd = view.split.botright("%40"),
+          }
+        }
+      )
+      vim.cmd('IronRepl')
     end, {silent = true})
 
-    vim.keymap.set('n', '<space>rs', '<cmd>IronRepl<cr>')
-    vim.keymap.set('n', '<space>rr', '<cmd>IronRestart<cr>')
-    vim.keymap.set('n', '<space>rf', '<cmd>IronFocus<cr>')
-    vim.keymap.set('n', '<space>rh', '<cmd>IronHide<cr>')
+    vim.keymap.set('n', '<space>rv', function()
+      iron.setup(
+        {
+          config = {
+            repl_open_cmd = view.split.vertical.botright("%40"),
+          }
+        }
+      )
+      vim.cmd('IronRepl')
+    end, {silent = true})
+
+
+    vim.keymap.set('n', '<space>sp', function()
+      vim.cmd('normal! vip')
+      iron.visual_send()
+    end, {silent = true})
+
+
+    vim.keymap.set('n', '<space>rt', '<cmd>IronRepl<cr>')
+    vim.keymap.set('n', '<space>rR', '<cmd>IronRestart<cr>')
   end
 }
