d550c6ec3d015501088bf1166c4ac79520908f67
nickeisenberg <nickeisenberg@gmail.com>
2024-03-27 10:17:33 -0700

    experimental iron function

--- config/nvim/lua/plugins/iron.lua
+++ config/nvim/lua/plugins/iron.lua
@@ -27,6 +27,22 @@ return {
       ignore_blank_lines = true,
     }
 
+    -- Not ready
+    -- See the source code for core.repl_for.
+    -- Need to mess with the visability to toggle the repl
+    local function _repl_below()
+      local ft = vim.bo[vim.api.nvim_get_current_buf()].filetype
+      local orig_win_height = vim.api.nvim_win_get_height(0)
+      local new_win_height = math.floor(orig_win_height * 0.75)
+      vim.cmd("split")
+      vim.cmd("resize " .. new_win_height)
+      vim.cmd("wincmd j")
+      iron.repl_here(ft)
+      vim.cmd("wincmd k")
+    end
+    vim.api.nvim_create_user_command('ReplBelow', _repl_below, {})
+
+
     vim.keymap.set('n', '<space>rh', function()
       iron.setup(
         {
@@ -37,6 +53,7 @@ return {
       )
       vim.cmd('IronRepl')
     end, {silent = true})
+
 
     vim.keymap.set('n', '<space>rv', function()
       iron.setup(
@@ -49,11 +66,6 @@ return {
       vim.cmd('IronRepl')
     end, {silent = true})
 
-
-    -- vim.keymap.set('n', '<space>sp', function()
-    --   vim.cmd('normal! vip')
-    --   iron.visual_send()
-    -- end, {silent = true})
 
     vim.keymap.set('n', '<space>sp', function()
       vim.cmd('normal! vip')
