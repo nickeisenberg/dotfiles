5cc499508bf3902614a1b2bee3fab27ab65eb5b6
nickeisenberg <nickeisenberg@gmail.com>
2024-04-02 13:50:00 -0700

    iron path

--- config/nvim/lua/plugins/iron.lua
+++ config/nvim/lua/plugins/iron.lua
@@ -1,6 +1,6 @@
 return {
   -- "Vigemus/iron.nvim",
-  dir = "/home/nicholas/GitRepos/iron.nvim",
+  dir = "/home/nicholas/GitRepos/iron_nvim/iron.nvim",
   branch = "master",
   config = function()
     local iron = require("iron.core")
