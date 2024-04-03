2288988d2756565010e55975ad5825ef58139e97
nickeisenberg <nickeisenberg@gmail.com>
2024-04-03 10:01:30 -0700

    some dapui bug. not sure. have to uninstall and reinstalll

--- config/nvim/lua/plugins/dap-ui.lua
+++ config/nvim/lua/plugins/dap-ui.lua
@@ -1,7 +1,8 @@
 return {
   "rcarriga/nvim-dap-ui",
   dependencies = {
-    "mfussenegger/nvim-dap"
+    "mfussenegger/nvim-dap",
+    "nvim-neotest/nvim-nio"
   },
   keys = {
   	{
@@ -10,24 +11,5 @@ return {
   		desc = "Toggle Debugger UI",
   	},
   },
-  -- automatically open/close the DAP UI when starting/stopping the debugger
-  config = function()
-    local dapui = require("dapui")
-    dapui.setup()
-
-    local dap = require("dap")
-
-    -- dap.listeners.before.attach.dapui_config = function()
-    --   dapui.open()
-    -- end
-    -- dap.listeners.before.launch.dapui_config = function()
-    --   dapui.open()
-    -- end
-    -- dap.listeners.before.event_terminated.dapui_config = function()
-    --   dapui.close()
-    -- end
-    -- dap.listeners.before.event_exited.dapui_config = function()
-    --   dapui.close()
-    -- end
-  end,
+  opts = {}
 }
--- config/nvim/lua/plugins/iron.lua
+++ config/nvim/lua/plugins/iron.lua
@@ -1,6 +1,6 @@
 return {
   -- "Vigemus/iron.nvim",
-  dir = "/home/nicholas/GitRepos/iron_nvim/iron.nvim",
+  dir = "/home/nicholas/GitRepos/iron.nvim/iron.nvim",
   branch = "master",
   config = function()
     local iron = require("iron.core")
