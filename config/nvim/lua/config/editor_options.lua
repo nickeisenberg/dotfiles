1848f9b912aad45bbd90caaf00a6f7354e8bc44f
Nicholas Eisenberg <EISENBNT@nv.doe.gov>
2024-04-18 12:16:17 -0700

    wsl copy and paste

--- config/nvim/lua/config/editor_options.lua
+++ config/nvim/lua/config/editor_options.lua
@@ -36,3 +36,18 @@ vim.diagnostic.config({
 })
 
 vim.keymap.set("n", "<leader>id", vim.diagnostic.open_float)
+
+if vim.fn.has('wsl') == 1 then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ['+'] = 'clip.exe',
+            ['*'] = 'clip.exe',
+        },
+        paste = {
+            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = 0,
+    }
+end
