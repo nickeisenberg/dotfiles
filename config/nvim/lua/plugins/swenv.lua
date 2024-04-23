5f9ac50548dcbdeddafcbf7ce416a3870fcb2cf5
nickeisenberg <nickeisenberg@gmail.com>
2024-04-23 15:06:57 -0700

    venv selector edits

--- config/nvim/lua/plugins/swenv.lua
+++ config/nvim/lua/plugins/swenv.lua
@@ -9,6 +9,27 @@ return {
       post_set_venv = nil,
     })
 
+
+    local function find_git_root()
+      local path = vim.fn.expand('%:p:h')
+      local root_marker = '.git'
+
+      while path ~= '/' and path ~= '' do
+          if vim.fn.isdirectory(path .. '/' .. root_marker) ~= 0 then
+              return path
+          end
+          path = vim.fn.fnamemodify(path, ':h')
+      end
+    end
+
+    local function listPyEnvsInCWD()
+      local settings = require('swenv.config').settings
+      settings.venvs_path = vim.fn.getcwd()
+      require('swenv.api').pick_venv()
+      vim.cmd('LspRestart')
+    end
+    vim.api.nvim_create_user_command('VenvCWD', listPyEnvsInCWD, {})
+
     local function listPy311EnvsAndSource()
       local settings = require('swenv.config').settings
       settings.venvs_path = vim.fn.expand('~/.venv311')
