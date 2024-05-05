b7fc46e5c4eea1caef54ea14a8f732051a6c4dae
Nick Eisenberg <nickeisenberg@gmail.com>
2024-05-04 20:18:56 -0700

    added some iron python setting if on windows

--- config/nvim/lua/plugins/iron.lua
+++ config/nvim/lua/plugins/iron.lua
@@ -1,16 +1,28 @@
 return {
   -- "Vigemus/iron.nvim",
   -- dir = "~/GitRepos/iron.nvim/iron.nvim",
+  -- dir = "~\\GitRepos\\iron.nvim",
   -- branch = "master",
   "nickeisenberg/iron.nvim",
   config = function()
     local iron = require("iron.core")
     local view = require("iron.view")
+    local isWindows = require("iron.util.os").isWindows
+
+    local repl_definition = {}
+    if isWindows() then
+      repl_definition = {
+        python = {
+          command = { "ipython", "--no-autoindent" }
+        }
+      }
+    end
 
     iron.setup {
       config = {
         scratch_repl = true,
         repl_open_cmd = view.split.vertical.rightbelow("%40"),
+        repl_definition = repl_definition
       },
       keymaps = {
         send_line = "<space>sl",
