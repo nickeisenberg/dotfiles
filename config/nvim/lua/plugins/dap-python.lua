a1103eb057c2ec5b9bc892e2c923f9c2557127f3
Nick Eisenberg <nickeisenberg@gmail.com>
2024-05-12 19:22:52 -0700

    added dap to windows

--- config/nvim/lua/plugins/dap-python.lua
+++ config/nvim/lua/plugins/dap-python.lua
@@ -2,7 +2,17 @@ return {
   "mfussenegger/nvim-dap-python",
   dependencies = "mfussenegger/nvim-dap",
   config = function()
-    local debugpyPythonPath = os.getenv("HOME") .. "/.venv311/debugpy/bin/python"
+
+    function isWindowsOS()
+        return package.config:sub(1,1) == '\\'
+    end
+    
+    if not isWindowsOS() then
+      local debugpyPythonPath = os.getenv("HOME") .. "/.venv311/debugpy/bin/python"
+    else
+      local debugpyPythonPath = "C:\\Users\\nicke\\venvs\\debugpy\\Scripts\\python.exe"
+    end
+
     print(debugpyPythonPath)
   	require("dap-python").setup(debugpyPythonPath)
   end,
