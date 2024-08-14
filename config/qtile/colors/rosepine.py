503afb279eff360bb993a2a914ed81ab165372fb
nickeisenberg <nickeisenberg@gmail.com>
2024-08-13 20:53:09 -0700

    moved the colors to their own file

--- config/qtile/colors/rosepine.py
+++ config/qtile/colors/rosepine.py
@@ -1,21 +1,28 @@
-colors = {
-	"_eperimental_nc": '#16141f',
-	"nc": '#16141f',
-	"base": '#191724',
-	"surface": '#1f1d2e',
-    "overlay": '#26233a',
-	"muted": '#6e6a86',
-	"subtle": '#908caa',
-	"text": '#e0def4',
-	"love": '#eb6f92',
-	"gold": '#f6c177',
-	"rose": '#ebbcba',
-    "muted_love": "#db4b4b",
-	"pine": '#31748f',
-	"foam": '#9ccfd8',
-	"iris": '#c4a7e7',
-	"highlight_low": '#21202e',
-	"highlight_med": '#403d52',
-    "highlight_high": '#524f67',
-    "none": 'NONE'
-}
+class Colors:
+    colors = {
+    	"_eperimental_nc": '#16141f',
+    	"nc": '#16141f',
+    	"base": '#191724',
+    	"surface": '#1f1d2e',
+        "overlay": '#26233a',
+    	"muted": '#6e6a86',
+    	"subtle": '#908caa',
+    	"text": '#e0def4',
+    	"love": '#eb6f92',
+    	"gold": '#f6c177',
+    	"rose": '#ebbcba',
+        "muted_love": "#db4b4b",
+    	"pine": '#31748f',
+    	"foam": '#9ccfd8',
+    	"iris": '#c4a7e7',
+    	"highlight_low": '#21202e',
+    	"highlight_med": '#403d52',
+        "highlight_high": '#524f67',
+        "none": 'NONE'
+    }
+    barcolor = colors["highlight_med"]
+    widget_background = colors["highlight_med"]
+    widget_text_color = colors["text"]
+    urgent = colors["love"]
+    muted_urgent = colors['muted_love']
+    selected = colors["rose"]
--- config/qtile/config.py
+++ config/qtile/config.py
@@ -19,44 +19,24 @@ from utils import (
     go_to_group,
     go_to_group_and_move_window
 )
-
+from colors.rosepine import Colors
 
 mod0 = "mod1"  # alt
 mod1 = "mod4"  # super
-
 terminal = 'alacritty'
-
 browser = "firefox"
 
-colors = {
-	"_eperimental_nc": '#16141f',
-	"nc": '#16141f',
-	"base": '#191724',
-	"surface": '#1f1d2e',
-    "overlay": '#26233a',
-	"muted": '#6e6a86',
-	"subtle": '#908caa',
-	"text": '#e0def4',
-	"love": '#eb6f92',
-	"gold": '#f6c177',
-	"rose": '#ebbcba',
-    "muted_love": "#db4b4b",
-	"pine": '#31748f',
-	"foam": '#9ccfd8',
-	"cyan": '#ebbcba',
-	"iris": '#c4a7e7',
-	"highlight_low": '#21202e',
-	"highlight_med": '#403d52',
-    "highlight_high": '#524f67',
-    "none": 'NONE'
-}
+#--------------------------------------------------
+# color setup
+#--------------------------------------------------
+colors = Colors()
 
-barcolor = colors["highlight_med"]
-widget_background = colors["highlight_med"]
-widget_text_color = colors["text"]
-urgent = colors["love"]
-muted_urgent = colors['muted_love']
-selected = colors["rose"]
+barcolor = colors.barcolor
+widget_background = colors.widget_background
+widget_text_color = colors.widget_text_color
+urgent = colors.urgent
+muted_urgent = colors.muted_urgent
+selected = colors.selected
 
 #--------------------------------------------------
 # Key Bindings 
