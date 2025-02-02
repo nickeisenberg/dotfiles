a6699403010fb514a1a28f89f0c2555f9dfbdf2b
nickeisenberg <nickeisenberg@gmail.com>
2025-02-02 11:50:00 -0800

    oil git notification

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -1,21 +1,26 @@
 return {
-  'stevearc/oil.nvim',
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
+  {
+    'stevearc/oil.nvim',
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("oil").setup({
+        view_options = {
+          show_hidden = true
+        },
+        win_options = {
+          signcolumn = "yes:2",
+        },
+      })
+      vim.keymap.set("n", "<space>O", "<cmd>Oil<cr>")
+    end
   },
-  config = function()
-    require("oil").setup({
-      view_options = {
-        show_hidden = true
-      },
-      columns = {
-        "icon",        -- Show file type icons
-        "permissions", -- Show file permissions
-        "size",        -- Show file size
-        "mtime",       -- Show modified time
-        "git_status",  -- Show Git status (modified, added, conflicts, etc.)
-      },
-    })
-    vim.keymap.set("n", "<space>O", "<cmd>Oil<cr>")
-  end
+  {
+    "refractalize/oil-git-status.nvim",
+    dependencies = {
+      "stevearc/oil.nvim",
+    },
+    config = true
+  }
 }
