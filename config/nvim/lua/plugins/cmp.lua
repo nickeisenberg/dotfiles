89dbd596e8ff56329abf58808c8c619eaceda199
nickeisenberg <nickeisenberg@gmail.com>
2025-02-10 09:39:01 -0800

    small refactor for lsp

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -6,28 +6,22 @@ return {
     -- Snippet Engine & its associated nvim-cmp source
     'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
-
-    -- Adds LSP completion capabilities
-    'hrsh7th/cmp-nvim-lsp',
-
-    -- Adds cmdline completion capabilities
-    'hrsh7th/cmp-cmdline',
-
-    -- Adds path completino
-    'hrsh7th/cmp-path',
-
-    -- Adds a number of user-friendly snippets
     'rafamadriz/friendly-snippets',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/cmp-path',
   },
   config = function()
-    local cmp = require 'cmp'
-    local luasnip = require 'luasnip'
+    local cmp = require('cmp')
+    local luasnip = require('luasnip')
+
     require('luasnip.loaders.from_vscode').lazy_load()
-    luasnip.config.setup {}
+
+    luasnip.config.setup({})
 
     luasnip.filetype_extend("htmldjango", {"html"})
 
-    cmp.setup {
+    cmp.setup({
       snippet = {
         expand = function(args)
           luasnip.lsp_expand(args.body)
@@ -50,22 +44,18 @@ return {
         { name = 'buffer' },
         { name = 'path' },
       },
-    }
+    })
 
     cmp.setup.cmdline(
       ':',
       {
         mapping = cmp.mapping.preset.cmdline(),
         sources = cmp.config.sources(
-          {
-            { name = 'path' }
-          },
+          { { name = 'path' } },
           {
             {
               name = 'cmdline',
-              option = {
-                ignore_cmds = { 'Man', '!' }
-              }
+              option = { ignore_cmds = { 'Man', '!' } }
             }
           }
         )
@@ -73,4 +63,3 @@ return {
     )
   end
 }
-
--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -32,11 +32,17 @@ return {
         end,
         { desc = 'Format current buffer with LSP' }
       )
+      vim.keymap.set(
+        'n', '<leader>F', ":Format<cr>",
+        { buffer = bufnr, desc = '[D]ocument [S]ymbols' }
+      )
     end
-
-    require('mason').setup()
-
+    
+    local mason = require("mason")
     local mason_lspconfig = require('mason-lspconfig')
+    local lspconfig = require("lspconfig")
+
+    mason.setup()
     mason_lspconfig.setup()
 
     local servers = {
@@ -57,23 +63,22 @@ return {
       },
     }
 
-    require('neodev').setup()
+    local capabilities = require('cmp_nvim_lsp').default_capabilities(
+      vim.lsp.protocol.make_client_capabilities()
+    )
 
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
-    mason_lspconfig.setup {
+    mason_lspconfig.setup({
       ensure_installed = vim.tbl_keys(servers),
-    }
+    })
 
     mason_lspconfig.setup_handlers {
       function(server_name)
-        require('lspconfig')[server_name].setup {
+        lspconfig[server_name].setup({
           capabilities = capabilities,
           on_attach = on_attach,
           settings = servers[server_name],
           filetypes = (servers[server_name] or {}).filetypes,
-        }
+        })
       end,
     }
   end
