3f980eb25ff12468a0946954b984e68e521cacfb
nicholas eisenberg <nickeisenberg@gmail.com>
2025-04-30 11:19:36 -0700

    added yarn option for installing markdown preview as this makes it work on wsl

--- config/nvim/lua/plugins/markdown-preview.lua
+++ config/nvim/lua/plugins/markdown-preview.lua
@@ -1,6 +1,18 @@
-return {
+local without_yarn = {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
   build = function() vim.fn["mkdp#util#install"]() end,
 }
+
+local with_yarn = {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
+}
+
+return with_yarn
