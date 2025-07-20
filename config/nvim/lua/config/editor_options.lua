f5478fdae7d9e9baca7ede624de1cb5d6ebe4e7b
NickEisenberg <nickeisenberg@gmail.com>
2025-07-20 11:21:17 -0700

    clipboatd for nvim

--- config/nvim/lua/config/editor_options.lua
+++ config/nvim/lua/config/editor_options.lua
@@ -38,8 +38,23 @@ vim.diagnostic.config({
   severity_sort = false,
 })
 
+-- Check if running over SSH or inside a remote terminal
+local is_ssh = vim.env.SSH_CONNECTION or vim.env.SSH_CLIENT
 
-if vim.fn.has('wsl') == 1 then
+if is_ssh then
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+  }
+
+elseif vim.fn.has('wsl') == 1 then
   vim.g.clipboard = {
     name = 'WslClipboard',
     copy = {
