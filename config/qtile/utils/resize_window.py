ad8d72667959f70be23c322703d13aae34fa463a
NickEisenberg <nickeisenberg@gmail.com>
2025-10-10 18:05:00 -0700

    ruff formatting on the qtile configs

--- config/qtile/utils/resize_window.py
+++ config/qtile/utils/resize_window.py
@@ -1,8 +1,8 @@
 from libqtile.lazy import lazy as clazy
 
 
-@clazy.window.function 
-def resize_floating_window(window, width: int = 0, height: int = 0): 
+@clazy.window.function
+def resize_floating_window(window, width: int = 0, height: int = 0):
     """
     Resize the floating windows
     """
@@ -11,8 +11,9 @@ def resize_floating_window(window, width: int = 0, height: int = 0):
     x_loc, y_loc = window.cmd_get_position()
     window.cmd_set_position_floating(x_loc - width, y_loc - height)
 
-@clazy.window.function 
-def grow_left_floating_window(window, width: int=0): 
+
+@clazy.window.function
+def grow_left_floating_window(window, width: int = 0):
     """
     Grow the floating windows left
     """
@@ -20,22 +21,25 @@ def grow_left_floating_window(window, width: int=0):
     x_loc, y_loc = window.cmd_get_position()
     window.cmd_set_position_floating(x_loc - width, y_loc)
 
-@clazy.window.function 
-def grow_right_floating_window(window, width: int=0): 
+
+@clazy.window.function
+def grow_right_floating_window(window, width: int = 0):
     """
     Grow the floating windows right
     """
     window.cmd_resize_floating(width, 0)
 
-@clazy.window.function 
-def grow_down_floating_window(window, height: int=0): 
+
+@clazy.window.function
+def grow_down_floating_window(window, height: int = 0):
     """
     Grow the floating windows down
     """
     window.cmd_resize_floating(0, height)
 
-@clazy.window.function 
-def grow_up_floating_window(window, height: int=0): 
+
+@clazy.window.function
+def grow_up_floating_window(window, height: int = 0):
     """
     Grow the floating windows up
     """
