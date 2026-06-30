6e87473da45aeb3fdf5c39e9cbc64df7fb34fd38
nicholas <nicholas@localhost.localdomain>
2026-06-30 17:41:27 -0400

    fix: qtile helpers work with newest qtile

--- config/qtile/utils/move_windows_to_group.py
+++ config/qtile/utils/move_windows_to_group.py
@@ -12,46 +12,65 @@ def debug_log(x, get="type"):
         f.write("\n")
     return None
 
+def _group_toscreen(group) -> None:
+    """
+    Compatibility wrapper for Qtile's group.toscreen() API.
+
+    Older versions of Qtile expose:
+        group.cmd_toscreen()
+
+    Newer versions expose:
+        group.toscreen()
+    """
+    if hasattr(group, "toscreen"):
+        group.toscreen()
+    else:
+        group.cmd_toscreen()
+
 
 def go_to_group(name: str, maingroups: list[Group]):
     """
-    See "How can I get my groups to stick to screens?" on the following link
+    See "How can I get my groups to stick to screens?" on the following link:
     https://docs.qtile.org/en/latest/manual/faq.html
     """
 
     def _inner(qtile: Qtile):
+        group = qtile.groups_map[name]
+
         if len(qtile.screens) == 1:
-            qtile.groups_map[name].cmd_toscreen()
+            _group_toscreen(group)
             return
 
         if name in np.arange(1, len(maingroups) + 1).astype(str):
             qtile.focus_screen(0)
-            qtile.groups_map[name].cmd_toscreen()
+            _group_toscreen(group)
         else:
             qtile.focus_screen(1)
-            qtile.groups_map[name].cmd_toscreen()
+            _group_toscreen(group)
 
     return _inner
 
 
 def go_to_group_and_move_window(name: str, maingroups: list[Group]):
     """
-    See "How can I get my groups to stick to screens?" on the following link
+    See "How can I get my groups to stick to screens?" on the following link:
     https://docs.qtile.org/en/latest/manual/faq.html
     """
 
     def _inner(qtile: Qtile):
+        group = qtile.groups_map[name]
+
         if len(qtile.screens) == 1:
             qtile.current_window.togroup(name, switch_group=True)
             return
 
+        qtile.current_window.togroup(name, switch_group=False)
+
         if name in np.arange(1, len(maingroups) + 1).astype(str):
-            qtile.current_window.togroup(name, switch_group=False)
             qtile.focus_screen(0)
-            qtile.groups_map[name].cmd_toscreen()
+            _group_toscreen(group)
         else:
-            qtile.current_window.togroup(name, switch_group=False)
             qtile.focus_screen(1)
-            qtile.groups_map[name].cmd_toscreen()
+            _group_toscreen(group)
 
     return _inner
