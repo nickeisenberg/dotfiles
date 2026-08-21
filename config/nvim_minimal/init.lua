8ae3ac7451db47b0799f05e794f07d8edd439744
nicholas <nicholas@localhost.localdomain>
2026-08-21 13:39:44 -0400

    added iron nvim to minimal nvim

--- config/nvim_minimal/init.lua
+++ config/nvim_minimal/init.lua
@@ -1,11 +1,8 @@
 vim.g.mapleader = " "
 
--- Add locally developed plugins.
-local plugin_root = vim.fn.stdpath("config") .. "/plugins"
-vim.opt.runtimepath:prepend(plugin_root .. "/picker.nvim")
-
 -- Load configuration.
 require("core")
 require("ui")
 require("coding")
 require("plugins")
+require("utils")
--- config/nvim_minimal/lua/plugins/init.lua
+++ config/nvim_minimal/lua/plugins/init.lua
@@ -1,3 +1,15 @@
 -- lua/plugins/init.lua
 
+-- Add locally developed plugins.
+local plugin_root = vim.fn.stdpath("config") .. "/plugins"
+vim.opt.runtimepath:prepend(plugin_root .. "/picker.nvim")
+
+-- Add git repo plugins.
+vim.pack.add({
+  {
+    src = "https://github.com/Vigemus/iron.nvim",
+  },
+})
+
 require("plugins.picker")
+require("plugins.iron")
