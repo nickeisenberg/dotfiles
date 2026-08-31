740efbccf4b54bf08c879818f7c17ceeae45288f
NickEisenberg <nickeisenberg@gmail.com>
2026-08-30 17:14:11 -0700

    minimal nvim added highlight symbol under cursor

--- config/nvim_minimal/lua/coding/lsp.lua
+++ config/nvim_minimal/lua/coding/lsp.lua
@@ -4,8 +4,12 @@ vim.lsp.enable({
   "bashls",
 })
 
+vim.opt.updatetime = 250
+
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+
     local function map(mode, lhs, rhs, desc)
       vim.keymap.set(mode, lhs, rhs, {
         buffer = ev.buf,
@@ -18,6 +22,26 @@ vim.api.nvim_create_autocmd("LspAttach", {
     map("n", "<leader>rn", vim.lsp.buf.rename, "LSP: Rename")
     map("n", "<leader>ca", vim.lsp.buf.code_action, "LSP: Code action")
     map("i", "<C-k>", vim.lsp.buf.signature_help, "LSP: Signature help")
+
+    -- Highlight references to the symbol under the cursor.
+    if client and client:supports_method("textDocument/documentHighlight") then
+      local group = vim.api.nvim_create_augroup(
+        "lsp-highlight-" .. ev.buf,
+        { clear = true }
+      )
+
+      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+        buffer = ev.buf,
+        group = group,
+        callback = vim.lsp.buf.document_highlight,
+      })
+
+      vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+        buffer = ev.buf,
+        group = group,
+        callback = vim.lsp.buf.clear_references,
+      })
+    end
   end,
 })
 
